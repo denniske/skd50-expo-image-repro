aa89d002d74ef7b359747f721f88691107cb6871
Dennis Keil <dennis.keil10@gmail.com>
2024-01-20 10:01:05 +0100

    sdk50 expo image nativeviewref error

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,15 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import {Image} from "expo-image";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Tab One Yo</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image source={{uri: 'https://www.nexushub.co/images/aoe2techtree.png'}} style={{width: 200, height: 200}}/>
     </View>
   );
 }
